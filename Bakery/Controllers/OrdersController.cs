730d80ba0688bac145083e856ba3e21ed67ad9b3
Michelle Morin <morin2m@gmail.com>
2020-03-13 13:54:29 -0700

    add edit and update routes, rename path decorator for destroy method as multiple post reqests with same name created conflict

--- Bakery/Controllers/OrdersController.cs
+++ Bakery/Controllers/OrdersController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Bakery.Models;
 using System.Collections.Generic;
+using System;
 
 namespace Bakery.Controllers
 {
@@ -24,7 +25,50 @@ namespace Bakery.Controllers
       return View(model);
     }
 
+    [HttpGet("/vendors/{vendorId}/orders/{orderId}/edit")]
+    public ActionResult Edit(int vendorId, int orderId)
+    {
+      Order currentOrder = Order.Find(orderId);
+      Vendor currentVendor = Vendor.Find(vendorId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("vendor", currentVendor);
+      model.Add("order", currentOrder);
+      return View(model);
+    }
+
     [HttpPost("/vendors/{vendorId}/orders/{orderId}")]
+    public ActionResult Update(string strOrderId, string strVendorId, string orderTitle, string orderDescription, string stringOrderPrice, string orderDate, string paid)
+    {
+      int orderId = int.Parse(strOrderId);
+      int vendorId = int.Parse(strVendorId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order foundOrder = Order.Find(orderId);
+      try
+      {
+        if (String.IsNullOrWhiteSpace(orderTitle) || String.IsNullOrWhiteSpace(orderDescription) || String.IsNullOrWhiteSpace(orderDate) || String.IsNullOrWhiteSpace(stringOrderPrice) || String.IsNullOrWhiteSpace(paid))
+        {
+          throw new System.InvalidOperationException("invalid input");
+        }
+        else
+        {
+          foundOrder.Title = orderTitle;
+          foundOrder.Description = orderDescription;
+          foundOrder.Date = orderDate;
+          foundOrder.Price = double.Parse(stringOrderPrice);
+          foundOrder.Paid = paid;
+          model.Add("vendor", foundVendor);
+          model.Add("order", foundOrder);
+          return View("Show", model);
+        }
+      }
+      catch (Exception ex)
+      {
+        return View("Error", ex.Message);
+      }
+    }
+
+    [HttpPost("/vendors/{vendorId}/orders/{orderId}/delete")]
     public ActionResult Destroy(string strOrderId, string strVendorId)
     {
       int orderId = int.Parse(strOrderId);
