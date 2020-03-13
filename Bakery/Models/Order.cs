961d991088fbeb43ebcb1e94aabc6283d4300d2e
Michelle Morin <morin2m@gmail.com>
2020-03-13 08:41:44 -0700

    add business logic for order class

---
+++ Bakery/Models/Order.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+
+namespace Bakery.Models
+{
+  public class Order
+  {
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+    public string Date { get; set; }
+    public int Id { get; }
+    private static int _counter = 1;
+    private static List<Order> _orders = new List<Order> {};
+
+    public Order(string title, string description, int price, string date)
+    {
+      Title = title;
+      Description = description;
+      Price = price;
+      Date = date;
+      _orders.Add(this);
+      Id = _counter;
+      IncreaseId();
+    }
+
+    public static void IncreaseId()
+    {
+      _counter++;
+    }
+
+    public static List<Order> GetAll()
+    {
+      return _orders;
+    }
+
+    public static void ClearAll()
+    {
+      _orders.Clear();
+    }
+
+    public static Order Find(int searchId)
+    {
+      for (int i=0; i< _orders.Count; i++)
+      {
+        if(_orders[i] is Order)
+        {
+          if(_orders[i].Id == searchId)
+          {
+            return _orders[i];
+          }
+        }
+      }
+      return null;
+    }
+
+    public static void DeleteOrder(int searchId)
+    {
+      for (int i=0; i< _orders.Count; i++)
+      {
+        if(_orders[i] is Order)
+        {
+          if(_orders[i].Id == searchId)
+          {
+            _orders.RemoveAt(i);
+          }
+        }
+      }
+    }
+
+  }
+}
