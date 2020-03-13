78a8e4defca735f0bb60637aa0e17f196a563b04
Michelle Morin <morin2m@gmail.com>
2020-03-13 08:52:41 -0700

    add business logic for vendor class

---
+++ Bakery/Models/Vendor.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+
+namespace Bakery.Models
+{
+  public class Vendor
+  {
+    private static List<Vendor> _vendors = new List<Vendor> {};
+    private static int _counter = 1;
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Id { get; }
+    public List<Order> Orders { get; set; }
+
+    public Vendor(string name, string description)
+    {
+      Name = name;
+      Description = description;
+      Id = _counter;
+      Orders = new List<Order> {};
+      IncreaseCounter();
+    }
+
+    public static IncreaseCounter()
+    {
+      _counter++;
+    }
+
+    public static void ClearAll()
+    {
+      _vendors.Clear();
+    }
+
+    public static List<Vendor> GetAll()
+    {
+      return _vendors;
+    }
+
+    public static Vendor Find(int searchId)
+    {
+      for (int i=0; i< _vendors.Count; i++)
+      {
+        if(_vendors[i] is Vendor)
+        {
+          if(_vendors[i].Id == searchId)
+          {
+            return _vendors[i];
+          }
+        }
+      }
+      return null;
+    }
+
+    public void AddOrder(Order order)
+    {
+      Orders.Add(order);
+    }
+  }
+}
